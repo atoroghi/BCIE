a8756fd7c692c76965f2630bd03bcea0d0b14636
griffin_floto <gfloto@uoguelph.ca>
2022-04-11 23:11:08 -0400

    numpy support for data

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ class IDMap:
     def __init__(self):
         # make link and item maps
         self.link_map = self.get_link_map()
-        with open('data/dict.pkl', 'rb') as f:
+        with open('data/triplet_dict.pkl', 'rb') as f:
             self.item_map = pickle.load(f)
 
     def get_link_map(self):
@@ -37,28 +37,17 @@ class IDMap:
             o = 'Freebase ID: {}'.format(triplet[2])
 
         print('<{} -- {} -- {}>'.format(s,r,o))
-        
-#kg = np.load('data/kg.npy', allow_pickle=True)
-#rec = np.load('data/rec.npy', allow_pickle=True)
 
-# read in files
-# TODO: just save the data as np files... reading is much faster
-print('reading train.txt')
-with open('datasets/ML_KG/train.txt') as f:
-    lines = f.readlines()
 
-triples = np.empty((len(lines), 3))
-for i, line in enumerate(lines):
-    t = line.strip().split('\t')
-    triples[i] = [t[0], t[1], t[2]]
+kg = np.load('data/kg.npy', allow_pickle=True)
+rec = np.load('data/rec.npy', allow_pickle=True)
+triples = np.concatenate((kg,rec), axis=0)
 
 triplet_map = IDMap()
 
 print('random triplets:')
-# both datasets are in identical format
 for i in range(15):
     ind = np.random.randint(triples.shape[0])
     triplet_map.convert(triples[ind])
-
 
 
