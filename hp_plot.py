15ed2f5c27889994a0b9e3644fce3a9877544bcd
griffin_floto <gfloto@uoguelph.ca>
2022-08-06 01:56:17 -0400

    plotting to show hp ranges

---
+++ hp_plot.py
@@ -0,0 +1,36 @@
+import os, sys, torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = 'gp'
+hps = [
+    'lr', 'batch size', 'emb dim', 'reg lambda', 
+    'kg lambda', 'init scale', 'neg ratio', 'neg_power'
+]
+
+# look at all exps
+for i, f in enumerate(os.listdir(path)):
+    exp_path = os.path.join(path, f)
+    print(f)
+
+    # for each fold
+    for j, g in enumerate(os.listdir(exp_path)):
+        load_path = os.path.join(exp_path, g)
+        x = torch.load(os.path.join(load_path, 'x_train.pt'))
+        y = torch.load(os.path.join(load_path, 'y_train.pt'))
+
+        x = x.numpy()
+        y = y.numpy()
+
+        # make plots
+        fig = plt.figure(figsize=(12,6))
+
+        for k in range(x.shape[1]):
+            s = int('2' + '4' + str(k+1))
+            ax = fig.add_subplot(s)
+
+            ax.scatter(x[:,k], y)
+            ax.set_title('{}: {}'.format(f, hps[k]))
+        plt.tight_layout()
+        plt.show()
+        break
