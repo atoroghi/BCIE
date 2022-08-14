54aad3a7b9aa4abfaec57a73ed5e8f1173209f2e
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2022-08-13 22:30:42 -0400

    Type checking added

--- dataload.py
+++ dataload.py
@@ -10,9 +10,11 @@ class DataLoader:
         self.sample_type = args.sample_type
         self.batch_size = args.batch_size
         self.fold = args.fold
+        self.type_checking = args.type_checking 
 
         # load datasets and info mappings
         path = os.path.join('datasets', self.name, 'fold {}'.format(self.fold))
+        main_path = os.path.join('datasets', self.name)
         self.rec_train = np.load(os.path.join(path, 'train.npy'), allow_pickle=True)
         self.rec_test = np.load(os.path.join(path, 'test.npy'), allow_pickle=True)
         self.rec_val = np.load(os.path.join(path, 'val.npy'), allow_pickle=True)
@@ -20,7 +22,6 @@ class DataLoader:
         # load data for training
         if args.kg == 'kg':
             self.kg = np.load(os.path.join('datasets', self.name, 'kg.npy'), allow_pickle=True)        
-        
             self.train_data = np.concatenate((self.rec_train, self.kg))    
             self.num_rel = np.max(self.kg[:,1]) + 1
             self.all_rel = np.unique(self.kg[:,1])
@@ -42,11 +43,31 @@ class DataLoader:
         #self.first_userid = np.min(self.users) # used for printing triplets
 
         # class for negative sampling
-        if self.sample_type == 'combo':
-            self.sampler = SingleSample(self.train_data, power=args.neg_power)
-        elif 'split' in self.sample_type:
-            self.sampler = DoubleSample(self.train_data, power=args.neg_power)
+        if args.type_checking == 'no':
+            if self.sample_type == 'combo':
+                self.sampler = SingleSample(self.train_data, power=args.neg_power)
+            elif 'split' in self.sample_type:
+                self.sampler = DoubleSample(self.train_data, power=args.neg_power)
 
+
+        if args.type_checking == 'check':
+            with open(os.path.join(main_path, 'valid_heads.pkl'), 'rb') as f:
+                self.valid_heads = pickle.load(f)
+            with open(os.path.join(main_path, 'valid_tails.pkl'), 'rb') as f:
+                self.valid_tails = pickle.load(f)
+            with open(os.path.join(main_path, 'valid_heads_freq.pkl'), 'rb') as f:
+                self.valid_heads_freq = pickle.load(f)
+            with open(os.path.join(main_path, 'valid_tails_freq.pkl'), 'rb') as f:
+                self.valid_tails_freq = pickle.load(f)
+
+
+            if self.sample_type == 'combo':
+                print("combo sample type not supported with type checking")
+                sys.exit()
+            elif 'split' in self.sample_type:
+                self.sampler = DoubleSampleType(self.valid_heads, self.valid_tails, self.valid_heads_freq,
+                self.valid_tails_freq, power=args.neg_power)
+            
         # user likes
         #with open(os.path.join(path, 'user_likes_test.pkl'), 'rb') as f:
         #    self.user_likes_map = pickle.load(f)
@@ -91,24 +112,32 @@ class DataLoader:
     # negative sampling
     def get_negatives(self, pos):
         n = self.neg_ratio * pos.shape[0] # number of neg samples
-        pos = np.repeat(np.copy(pos), self.neg_ratio, axis=0)
-        
-        # TODO: switch to only modify tail (to make similar to svd)
-        mask = np.random.randint(0, 2, size=(n))
-        mask = np.vstack((mask, np.ones(n), 1 - mask)).T
 
-        if self.sample_type == 'combo':
-            samples = self.sampler.sample(n)
-            samples = np.vstack((samples, np.zeros(n), samples)).T
-        # soft samples work better rn?
-        elif 'split' in self.sample_type:
-            # option to flip type
-            head_samples, tail_samples = self.sampler.sample(n)
-            
+        if self.type_checking == 'check':
+            head_samples, tail_samples = self.sampler.sample(pos,self.neg_ratio)
             if 'reg' in self.sample_type:
                 samples = np.vstack((head_samples, np.zeros(n), tail_samples)).T
             elif 'rev' in self.sample_type:
                 samples = np.vstack((tail_samples, np.zeros(n), head_samples)).T
+
+        pos = np.repeat(np.copy(pos), self.neg_ratio, axis=0)
+        # TODO: switch to only modify tail (to make similar to svd)
+        mask = np.random.randint(0, 2, size=(n))
+        mask = np.vstack((mask, np.ones(n), 1 - mask)).T
+
+        if self.type_checking == 'no':
+            if self.sample_type == 'combo':
+                samples = self.sampler.sample(n)
+                samples = np.vstack((samples, np.zeros(n), samples)).T
+            # soft samples work better rn?
+            elif 'split' in self.sample_type:
+                # option to flip type
+                head_samples, tail_samples = self.sampler.sample(n)
+                
+                if 'reg' in self.sample_type:
+                    samples = np.vstack((head_samples, np.zeros(n), tail_samples)).T
+                elif 'rev' in self.sample_type:
+                    samples = np.vstack((tail_samples, np.zeros(n), head_samples)).T
 
         neg = pos * mask + samples * (1 - mask)
         return neg
@@ -182,4 +211,32 @@ class DoubleSample:
         else:
             head_samples = np.random.choice(self.num_items_head + 1, size=(n), p=self.head_dist)
             tail_samples = np.random.choice(self.num_items_tail + 1, size=(n), p=self.tail_dist)
+        return head_samples, tail_samples
+
+class DoubleSampleType:
+    def __init__(self, valid_heads, valid_tails, valid_heads_freq, valid_tails_freq, power=0):
+        self.power = power
+        self.valid_heads = valid_heads
+        self.valid_tails = valid_tails
+        self.valid_heads_freq = valid_heads_freq
+        self.valid_tails_freq = valid_tails_freq
+
+    def sample(self, pos, neg_ratio):
+        head_samples = []
+        tail_samples = []
+        if self.power == 0:
+            for fact in pos:
+                _, rel, _ = fact
+                head_samples.append(np.random.choice(self.valid_heads[rel], size=(neg_ratio)))
+                tail_samples.append(np.random.choice(self.valid_tails[rel], size=(neg_ratio)))
+        # discrete inverse sampling
+        else:
+            for fact in pos:
+                _, rel, _ = fact
+                head_probs = self.valid_heads_freq[rel]
+                tail_probs = self.valid_rails_freq[rel]
+                head_samples.append(np.random.choice(self.valid_heads[rel], size=(neg_ratio), p=head_probs))
+                tail_samples.append((np.random.choice(self.valid_tails[rel], size=(neg_ratio), p=tail_probs)))
+        head_samples = np.array(head_samples).reshape(len(head_samples)*neg_ratio)
+        tail_samples = np.array(tail_samples).reshape(len(tail_samples)*neg_ratio)
         return head_samples, tail_samples
