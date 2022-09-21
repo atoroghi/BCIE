2f44301b6845dc3ec86274c455b705c344b25a18
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2022-09-21 14:11:49 -0400

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,45 +1,28 @@
 
 
 import math
-#import torch
-#from torch.distributions.multivariate_normal import MultivariateNormal
+import torch
 import numpy as np
 import cvxpy as cp
-#seed = torch.manual_seed(1)
 from scipy.special import expit
 import scipy.linalg as sp
 
 class Updater:
-	def __init__(self,X,y,mu_prior,Sigma_prior,W,args,etta,device):
-		#self.X = X.cpu()
+	def __init__(self,X,y,mu_prior,Sigma_prior,W,args,etta,device, update_type, likelihood_precision):
 		self.X = X
-		#self.y = y.cpu()
 		self.y = y
-		#self.mu_prior = mu_prior.cpu()
 		self.mu_prior = mu_prior
-		#self.Sigma_prior = Sigma_prior.cpu()
 
 		# this is precision not variance
 		self.tau_prior = tau_prior
-		#self.lam= torch.cholesky_inverse(Sigma_prior)
-		##zz , _ = sp.lapack.dpotrf(Sigma_prior, False, False)
-		##inv_M , _ = sp.lapack.dpotri(zz)
-		#self.lam = np.linalg.inv(Sigma_prior)
-		##self.lam = np.triu(inv_M) + np.triu(inv_M, k=1).T
-		#self.W= W.cpu()
 		self.W = W
-		#self.prior_distribution=MultivariateNormal(mu_prior, covariance_matrix=Sigma_prior)
-		##self.prior_distribution = np.random.multivariate_normal(mu_prior,Sigma_prior)
 		self.alpha=args.alpha
 		self.max_iters=args.max_iters_laplace
 		self.etta=etta
 		self.emb_dim=args.emb_dim
 		self.device = device
-		#self.session_no = session_no
-		#self.etta_dict={1:0.00001,2:0.1,3:0.5,4:1,5:10}
 		self.update_type = args.update_type
 		self.likelihood_precision = args.likelihood_precision
-		#self.etta_cvx = session_no
 
 
 
@@ -48,21 +31,14 @@ class Updater:
 
 		return self.W
 
-	##def SDR_cvxopt(self,Sigma_prior, X_all, y , previous_w):
 	def SDR_cvxopt(self,landa, X_all, y , previous_w):
-		#X_all = X_all.cpu()
-		#y = y.cpu()
-		##landa = np.linalg.inv(Sigma_prior)
-		#landa = torch.cholesky_inverse(Sigma_prior).cpu()
-		#previous_w=previous_w.cpu().detach().numpy()
+
 		w= cp.Variable(self.emb_dim)
 		constraints=[]
 		objective_function= cp.quad_form(w-previous_w, landa)
 		for i in range(len(X_all)):
 			var= (X_all[i]@w)*y[i]
-			#objective_function += cp.logistic(-1*self.etta*var)
 			objective_function += self.etta * cp.logistic(-1*var)
-			#objective_function += self.etta_cvx*cp.logistic(-1*var)
 		prob = cp.Problem(cp.Minimize(objective_function), constraints)
 		prob2 = cp.Problem(cp.Minimize(1000*objective_function),constraints)
 		try:
@@ -70,7 +46,6 @@ class Updater:
 		except:
 			prob2.solve()
 		
-		#return w.value , prob.value
 		return w.value
 
 	#def log_prior(self):
@@ -80,55 +55,34 @@ class Updater:
 #		return nlp, g
 
 	def log_likelihood(self):
-		#(N,_) = self.X.size()
 		N = np.shape(self.X)[0]
-		#tensorW=torch.tensor(self.W)
 		logits= np.matmul(self.X,self.W)
-		#logits = torch.mv(self.X, tensorW.float())
-		##probs1 = np.clip(expit(-1*self.y*logits*self.etta),1e-6,1-1e-6)
-		#probs1 = torch.sigmoid(-1*self.y*logits*self.etta).clamp(min=1e-6, max=1-1e-6)
 		probs = np.clip(expit(self.y*logits*self.etta),1e-6,1-1e-6)
-		#probs = torch.sigmoid(self.y*logits*self.etta).clamp(min=1e-6, max=1-1e-6)
-		#nll = -1* np.sum(np.log(probs))
-		#nll= -1 * torch.sum(torch.log(probs))
-		#g: gradient of negative log likelihood
-		#g= -1*np.matmul(np.transpose(self.X),self.y*probs1)
-		#g= -1*torch.mv(self.X.t(),self.y*probs1)
 		H= np.matmul(np.matmul(np.transpose(self.X),np.diag((probs*(1-probs)))),self.X)
 
-		#H = torch.mm(torch.mm(self.X.t(), torch.diag((probs * (1 - probs)))), self.X)
 		return H
 
 
 
 	def compute_laplace_approximation(self):
-		#print("Sigma prior:",self.Sigma_prior)
-		#print("mu_input",self.W)
+
 		assert args.update_type in ['gaussian', 'laplace']
 		if self.update_type == "gaussian":
 			n = self.X.size()[0]
-			#mu = (1/((1/(self.Sigma_prior^2)) + (n/(self.likelihood_variance^2)))) * (self.mu_prior/(self.Sigma_prior^2) + self.X.sum()/(self.likelihood_variance^2))
-			#H_out = (1/self.Sigma_prior^2) + (n/(self.likelihood_variance^2))
-			
+
 			H_out = self.prior_precision + n * self.likelihood_precision
 			mu = (self.prior_precision * self.mu_prior + self.likelihood_precision * self.X.sum()) * torch.cholesky_inverse(H_out)
 		if self.update_type == "laplace":
 
-			#W_new, _ = self.SDR_cvxopt(self.Sigma_prior, self.X, self.y , self.W)
 			W_new = self.SDR_cvxopt(self.tau_prior, self.X, self.y , self.W)
 			self.W = W_new
 			mu = self.W
-			#print("mu_out:",mu)
 			H_map = self.log_likelihood()
-			#print("H_map:",H_map)
+
 			prior_precision = self.tau_prior
 			za = prior_precision + self.etta*H_map
 			H_out=np.maximum(za,za.T)
-			#print("H_out:",H_out)
-			##za , _ = sp.lapack.dpotrf(prior_precision + self.etta*H_map, False, False)
-			##inv_A , _ = sp.lapack.dpotri(za)
-			##Sigma = np.triu(inv_A) + np.triu(inv_A, k=1).T
-			##Sigma = np.linalg.inv(prior_precision + self.etta*H_map)
+
 		return mu, H_out
 
 
