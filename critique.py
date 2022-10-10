71a6fd9cb24600505e80cc2d34437f219af208f7
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2022-10-09 21:11:39 -0400

    Update critique.py

--- critique.py
+++ critique.py
@@ -231,7 +231,11 @@ def critiquing(crit_args, mode):
                 # select a crit (user action) and remove it from pool
                 #crit_node, crit_pair = select_critique(ht_facts, rec_facts, crit_args.critique_mode, pop_counts, items_facts_tail_gt)
 
-                crit, ht_facts = beta_crit(ht_facts) # crit in (node, rel) format
+                try:
+                    crit, ht_facts = beta_crit(ht_facts) # crit in (node, rel) format
+                except:
+                    print(gt)
+                    sys.exit()
                 crit_rel_emb = rel_emb[crit[1]] 
 
                 # get d for p(user | d) bayesian update
