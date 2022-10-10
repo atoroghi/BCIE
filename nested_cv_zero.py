c94a458e33aa79d3c8e10950170b45f408699d07
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2022-10-09 23:55:10 -0400

    Update nested_cv_zero.py

--- nested_cv_zero.py
+++ nested_cv_zero.py
@@ -30,7 +30,7 @@ def get_metacrit_args():
     # other hyper-params
     parser.add_argument('-evidence_type', default='direct', type=str, help='direct or indirect')
     parser.add_argument('-critique_target', default='object', type=str, help='object or item')
-    parser.add_argument('-tune_name', default='dev', type=str, help="tuner process name")
+    parser.add_argument('-tune_name', default='gausstypereg', type=str, help="tuner process name")
     parser.add_argument('-update_type', default='gaussian', type=str, help='laplace or gaussian')
     parser.add_argument('-critique_mode', default='random', type=str, help='random or pop or diff')
 
