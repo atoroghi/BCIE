c10f138f6b8e3e1059e742f660373fc2fe46dfc2
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2022-10-15 09:00:53 -0400

    Update critique.py

--- critique.py
+++ critique.py
@@ -207,6 +207,7 @@ def critiquing(crit_args, mode):
 ##############################################################
     rank_track = None
     for i, user in enumerate(all_users):
+        print(i)
         if i == 10: break
 
         # get ids of top k recs, and all gt from user
