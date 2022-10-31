Gradient Descent Updater is Fixed

utils/updateinfo.py
@@ -10,7 +10,6 @@ def stack_(x):
 class UpdateInfo:
     def __init__(self, user_emb, etta, crit_args, model_args, device, crit_rel_emb=None, likes_emb=None):
         self.etta = 0.5 * np.array([1, 1, 1, 1, 1])
-        #self.etta = etta
         
         self.crit_rel_emb_f = None
         self.crit_rel_emb_inv = None
@@ -33,7 +32,7 @@ class UpdateInfo:
         self.user_prec_inv = torch.unsqueeze(prec, axis=0)
         
         # p(d | u)
-        self.likelihood_prec = 1 * torch.eye(model_args.emb_dim).to(device)
+        self.likelihood_prec = 1* torch.eye(model_args.emb_dim).to(device)
         #self.likelihood_prec = crit_args.default_prec * torch.eye(model_args.emb_dim).to(device)
 
         self.z_mean = torch.zeros(model_args.emb_dim).to(device)